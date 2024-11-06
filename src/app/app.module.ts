7168be42e98202cb0cd5618c48de360c3778adbe
danielcamisi <danielcamillo2010@hotmail.com>
2024-11-05 22:41:16 -0300

    Organizing the front-end of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';                    //imports cores
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';  //Some animations
+
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';                                    //import components
+import { HeaderComponent } from './pages/layout/header/header.component';
+import { FooterComponent } from './pages/layout/footer/footer.component';
+import { DashboardComponent } from './pages-adm/components/dashboard/dashboard.component';   
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './pages/shared/home/home.component';  //import Angular Material
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
